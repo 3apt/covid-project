c7d052a799df74756c0c56962a18b114f65d4316
3apt <bpt.moreau@outlook.fr>
2021-03-08 15:38:50 +0100

    ajout du troisieme prototype

---
+++ functions_covid.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  8 11:12:32 2021
+
+@author: baptiste
+"""
+
+import keras
+import tensorflow as tf
+import numpy as np
+from skimage import transform
+import cv2
+from keras.models import Model
+
+# algorithme de grad-CAM proposé par Keras : https://keras.io/examples/vision/grad_cam/
+def make_gradcam_heatmap(
+    img_array, model, last_conv_layer_name, classifier_layer_names
+):
+    # First, we create a model that maps the input image to the activations
+    # of the last conv layer
+    last_conv_layer = model.get_layer(last_conv_layer_name)
+    last_conv_layer_model = keras.Model(model.inputs, last_conv_layer.output)
+
+    # Second, we create a model that maps the activations of the last conv
+    # layer to the final class predictions
+    classifier_input = keras.Input(shape=last_conv_layer.output.shape[1:])
+    x = classifier_input
+    for layer_name in classifier_layer_names:
+        x = model.get_layer(layer_name)(x)
+    classifier_model = keras.Model(classifier_input, x)
+
+    # Then, we compute the gradient of the top predicted class for our input image
+    # with respect to the activations of the last conv layer
+    with tf.GradientTape() as tape:
+        # Compute activations of the last conv layer and make the tape watch it
+        last_conv_layer_output = last_conv_layer_model(img_array)
+        tape.watch(last_conv_layer_output)
+        # Compute class predictions
+        preds = classifier_model(last_conv_layer_output)
+        top_pred_index = tf.argmax(preds[0])
+        top_class_channel = preds[:, top_pred_index]
+
+    # This is the gradient of the top predicted class with regard to
+    # the output feature map of the last conv layer
+    grads = tape.gradient(top_class_channel, last_conv_layer_output)
+
+    # This is a vector where each entry is the mean intensity of the gradient
+    # over a specific feature map channel
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+
+    # We multiply each channel in the feature map array
+    # by "how important this channel is" with regard to the top predicted class
+    last_conv_layer_output = last_conv_layer_output.numpy()[0]
+    pooled_grads = pooled_grads.numpy()
+    for i in range(pooled_grads.shape[-1]):
+        last_conv_layer_output[:, :, i] *= pooled_grads[i]
+
+    # The channel-wise mean of the resulting feature map
+    # is our heatmap of class activation
+    heatmap = np.mean(last_conv_layer_output, axis=-1)
+
+    # For visualization purpose, we will also normalize the heatmap between 0 & 1
+    heatmap = np.maximum(heatmap, 0) / np.max(heatmap)
+    return heatmap
+
+def decode_predictions(pred):
+    if np.argmax(pred) == 1:
+        return 'covid'
+    elif np.argmax(pred) == 2:
+        return 'pneumo'
+    else:
+        return 'normal'
+    
+def get_img_array(img, im_shape):
+    X = img/255
+    X = transform.resize(X, im_shape)
+    return X.reshape((1, im_shape[0], im_shape[1], 1))
+
+
+# autre algorithme de Grad-CAM, pour superposer avec les images
+
+class GradCAM:
+    def __init__(self, model, classIdx, layerName=None):
+        # store the model, the class index used to measure the class
+        # activation map, and the layer to be used when visualizing
+        # the class activation map
+        self.model = model
+        self.classIdx = classIdx
+        self.layerName = layerName
+        # if the layer name is None, attempt to automatically find
+        # the target output layer
+        if self.layerName is None:
+            self.layerName = self.find_target_layer()
+            
+    def find_target_layer(self):
+        # attempt to find the final convolutional layer in the network
+        # by looping over the layers of the network in reverse order
+        for layer in reversed(self.model.layers):
+            # check to see if the layer has a 4D output
+            if len(layer.output_shape) == 4:
+                return layer.name
+        # otherwise, we could not find a 4D layer so the GradCAM
+        # algorithm cannot be applied
+        raise ValueError("Could not find 4D layer. Cannot apply GradCAM.")
+        
+    def compute_heatmap(self, image, eps=1e-8, normalize=True):
+        # construct our gradient model by supplying (1) the inputs
+        # to our pre-trained model, (2) the output of the (presumably)
+        # final 4D layer in the network, and (3) the output of the
+        # softmax activations from the model
+        gradModel = Model(inputs=[self.model.input], outputs=[self.model.get_layer(self.layerName).output,
+                                                              self.model.output])
+        # record operations for automatic differentiation
+        with tf.GradientTape() as tape:
+            # cast the image tensor to a float-32 data type, pass the
+            # image through the gradient model, and grab the loss
+            # associated with the specific class index
+            inputs = tf.cast(image, tf.float32)
+            inputs = np.expand_dims(inputs, axis=0)
+            (convOutputs, predictions) = gradModel(inputs)
+            loss = predictions[:, self.classIdx]
+        # use automatic differentiation to compute the gradients
+        grads = tape.gradient(loss, convOutputs)
+        # if gradients are too small (GradCAM is zero everywhere)
+        # equal to changing the value of 'eps' func arg
+        grads = grads / (grads.numpy().max() - grads.numpy().min())
+        # compute the guided gradients
+        castConvOutputs = tf.cast(convOutputs > 0, "float32")
+        castGrads = tf.cast(grads > 0, "float32")
+        guidedGrads = castConvOutputs * castGrads * grads
+        # the convolution and guided gradients have a batch dimension
+        # (which we don't need) so let's grab the volume itself and
+        # discard the batch
+        convOutputs = convOutputs[0]
+        guidedGrads = guidedGrads[0]
+        # compute the average of the gradient values, and using them
+        # as weights, compute the ponderation of the filters with
+        # respect to the weights
+        weights = tf.reduce_mean(guidedGrads, axis=(0, 1))
+        cam = tf.reduce_sum(tf.multiply(weights, convOutputs), axis=-1)
+        # grab the spatial dimensions of the input image and resize
+        # the output class activation map to match the input image
+        # dimensions
+        h, w = image.shape[:2]
+        heatmap = cv2.resize(cam.numpy(), (w, h))
+        #         print('avg heatmap value:', np.mean(heatmap))
+        #         print('max heatmap value:', np.max(heatmap))
+        # ignore certain values lower than a threshold to get sharper heatmaps
+        # heatmap[np.where(heatmap < 1)] = 0
+        if normalize:
+            # normalize the heatmap such that all values lie in the range
+            # [0, 1], scale the resulting values to the range [0, 255],
+            # and then convert to an unsigned 8-bit integer
+            numer = heatmap - np.min(heatmap)
+            denom = (heatmap.max() - heatmap.min()) + eps
+            heatmap = numer / denom
+            heatmap = (heatmap * 255)
+        return heatmap.astype("uint8")
+    
+    def overlay_heatmap(self, heatmap, image, alpha=0.5, colormap=cv2.COLORMAP_VIRIDIS):
+        # apply the supplied color map to the heatmap and then
+        # overlay the heatmap on the input image
+        heatmap = cv2.applyColorMap(heatmap, colormap)
+        # if the input image is grey-scale, convert it to 3-dim
+        if len(image.shape) == 2 or image.shape[-1] != 3:
+            image = cv2.cvtColor(src=image, code=cv2.COLOR_GRAY2RGB)
+        # if image px values are in [0, 1], upscale to [0, 255]
+        if np.max(image) <= 1.0:
+            image = image * 255.0
+        output = cv2.addWeighted(image.astype('uint8'), alpha, heatmap, 1 - alpha, 0)
+        return output
